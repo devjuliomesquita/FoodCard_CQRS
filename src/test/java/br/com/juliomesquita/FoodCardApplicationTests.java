e54049b8e324a236eb16db4df771d3b115a04841
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2023-11-23 00:14:15 -0300

    Initial commit

---
+++ src/test/java/br/com/juliomesquita/FoodCardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliomesquita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
